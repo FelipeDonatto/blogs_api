4197b07899703b692030367be6f7b228c081ed3b
FelipeDonatto <fe.donatto@outlook.com>
2022-10-25 18:26:01 -0300

    req 02

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+const usersModel = (sequelize, DataTypes) => {
+ const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+ })   
+return User;
+}
+
+module.exports = usersModel;
